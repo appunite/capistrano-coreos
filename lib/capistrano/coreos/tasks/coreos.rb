9a45a5c4f723bdefb27fe3852d343981b3030926
Seigo Uchida <spesnova@gmail.com>
2015-01-31 01:26:57 +0900

    Create coreos tasks file

---
+++ lib/capistrano/coreos/tasks/coreos.rb
@@ -0,0 +1,36 @@
+namespace :coreos do
+  desc "Show machine info"
+  task :machine do
+    machines = {}
+
+    on roles(:all) do |m|
+      machines[m.hostname] = {
+        version:  capture("cat /etc/os-release | grep VERSION= | sed s/VERSION=// "),
+        channel:  capture("cat /etc/coreos/update.conf | grep GROUP= | sed s/GROUP=// "),
+        strategy: capture("cat /etc/coreos/update.conf | grep REBOOT_STRATEGY= | sed s/REBOOT_STRATEGY=// "),
+      }
+    end
+
+    outputs = [["MACHINE", "VERSION", "CHANNEL", "STRATEGY"]]
+    machines.each do |k,v|
+      outputs << [k, v[:version], v[:channel], v[:strategy]]
+    end
+    outputs.each do |o|
+      puts o[0].ljust(15) + o[1].ljust(10) + o[2].ljust(10) + o[3].ljust(10)
+    end
+  end
+
+  desc "Update CoreOS"
+  task :update do
+    on roles(:all) do |m|
+      #sudo update_engine_client -update
+    end
+  end
+
+  desc "Reboot CoreOS"
+  task :reboot do
+    on roles(:all) do |m|
+      #reboot
+    end
+  end
+end
